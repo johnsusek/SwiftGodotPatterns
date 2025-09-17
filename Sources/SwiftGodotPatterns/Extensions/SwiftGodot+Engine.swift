293709786b14dbafe968fe1e01fe8f88876b871b
John Susek <john@johnsolo.net>
2025-09-16 19:52:23 -0500

    Add onNextFrame

--- Sources/SwiftGodotPatterns/Extensions/SwiftGodot+Engine.swift
+++ Sources/SwiftGodotPatterns/Extensions/SwiftGodot+Engine.swift
@@ -4,4 +4,14 @@ public extension Engine {
   static func getSceneTree() -> SceneTree? {
     Engine.getMainLoop() as? SceneTree
   }
+
+  @discardableResult
+  static func onNextFrame(_ f: @escaping () -> Void) -> Bool {
+    guard let tree = Engine.getMainLoop() as? SceneTree,
+          let timer = tree.createTimer(timeSec: 0.0) else { return false }
+    _ = timer.timeout.connect { f() }
+    return true
+  }
 }
+
+import SwiftGodot
