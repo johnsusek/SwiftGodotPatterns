4b09e5aad75b36bb2c983b571fd30ba7072d83cf
John Susek <john@johnsolo.net>
2025-10-03 19:04:20 -0500

    Add PatternRegistry.types for consuming games to call in their `register(type:)`. This allows seeing our components in the editor.

---
+++ Sources/SwiftGodotPatterns/Registration.swift
@@ -0,0 +1,19 @@
+import SwiftGodot
+
+public enum PatternsRegistry {
+  public static let types: [Object.Type] = [
+    AbilityRunnerComponent.self,
+    AseSprite.self,
+    ControlBrain2D.self,
+    HealthComponent2D.self,
+    Hitbox2D.self,
+    Knockback2DComponent.self,
+    LifetimeComponent2D.self,
+    PlayerController2D.self,
+    ResourceMirrorComponent.self,
+    ResourceSystem.self,
+    TargetScanner2D.self,
+    TweenOneShot.self,
+    Velocity2DComponent.self,
+  ]
+}
